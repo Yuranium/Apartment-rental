feecc53cbd6b922b74e949f6ead78ef62e945ad1
Yuranium <dolazifet@gmail.com>
2024-10-18 00:25:59 +0300

    User CRUD operations.

--- src/main/java/ru/yuriy/propertyrental/perositories/UserRepository.java
+++ src/main/java/ru/yuriy/propertyrental/perositories/UserRepository.java
@@ -2,10 +2,16 @@ package ru.yuriy.propertyrental.perositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.yuriy.propertyrental.models.entity.User;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +19,7 @@ public interface UserRepository extends JpaRepository<User, Long>
 {
     @Transactional(readOnly = true)
     Optional<User> findByEmailOrPhone(String email, String phone);
+
+    @Transactional(readOnly = true)
+    Optional<User> findByPhoneAndEmail(String phone, String email);
 }
--- src/main/java/ru/yuriy/propertyrental/services/UserService.java
+++ src/main/java/ru/yuriy/propertyrental/services/UserService.java
@@ -2,33 +2,28 @@ package ru.yuriy.propertyrental.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.BindingResult;
+import org.springframework.transaction.annotation.Transactional;
 import ru.yuriy.propertyrental.models.UserForm;
 import ru.yuriy.propertyrental.models.entity.User;
 import ru.yuriy.propertyrental.perositories.UserRepository;
 
-import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService
 {
     private final UserRepository userRepository;
-    public Boolean saveNewUser(UserForm userForm, BindingResult result)
+
+    @Transactional(readOnly = true)
+    public List<User> userList()
     {
-        if (!userForm.equalsPassword())
-        {
-            result.rejectValue("confirmPassword", "error.userForm", "Пароли не совпадают");
-            return Boolean.FALSE;
-        }
-        else
-        {
-            saveUser(userForm);
-            return Boolean.TRUE;
-        }
+        return userRepository.findAll();
     }
 
-    private void saveUser(UserForm userForm)
+    @Transactional(rollbackFor = {RuntimeException.class})
+    public void saveUser(UserForm userForm)
     {
         User user = new User();
         user.setName(userForm.getName());
@@ -36,8 +31,39 @@ public class UserService
         user.setEmail(userForm.getEmail());
         user.setPhone(userForm.getPhone());
         user.setPassword(userForm.getPassword());
-        user.setBirthday(new Date(userForm.getBirthday()));
+        user.setBirthday(userForm.getBirthday());
         user.setActive(Boolean.FALSE);
         // userRepository.save(user);
+    }
+
+    @Transactional
+    public boolean updateUser(UserForm userForm)
+    {
+        Optional<User> userOpt = userRepository.findByPhoneAndEmail(userForm.getPhone(), userForm.getEmail());
+        if (userOpt.isEmpty())
+            return false;
+        else
+        {
+            User user1 = userOpt.get();
+            user1.setName(userForm.getName());
+            user1.setLastName(userForm.getLastName());
+            user1.setEmail(userForm.getEmail());
+            user1.setPhone(userForm.getPhone());
+            user1.setPassword(userForm.getPassword());
+            return true;
+        }
+    }
+
+    @Transactional
+    public boolean deleteUser(UserForm deleteUser)
+    {
+        Optional<User> userOpt = userRepository.findByPhoneAndEmail(deleteUser.getPhone(), deleteUser.getEmail());
+        if (userOpt.isEmpty())
+            return false;
+        else
+        {
+            userRepository.deleteById(userOpt.get().getId());
+            return true;
+        }
     }
 }
