67e59a89a0e0b4284215908254fcef46d498af28
Yuranium <dolazifet@gmail.com>
2024-11-22 16:13:37 +0300

    Redis cache.

--- src/main/java/ru/yuriy/propertyrental/config/SecurityConfig.java
+++ src/main/java/ru/yuriy/propertyrental/config/SecurityConfig.java
@@ -3,8 +3,6 @@ package ru.yuriy.propertyrental.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,15 +48,6 @@ public class SecurityConfig
                         .permitAll()
                 )
                 .build();
-    }
-
-    //@Bean
-    public AuthenticationProvider provider()
-    {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(service);
-        provider.setPasswordEncoder(encoder());
-        return provider;
     }
 
     @Bean
