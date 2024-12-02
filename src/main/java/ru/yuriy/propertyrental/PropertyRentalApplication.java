de4a6418ab4c891518593113f26016dfa707a863
Yuranium <dolazifet@gmail.com>
2024-12-02 21:30:16 +0300

    .

--- src/main/java/ru/yuriy/propertyrental/PropertyRentalApplication.java
+++ src/main/java/ru/yuriy/propertyrental/PropertyRentalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PropertyRentalApplication {
-
-    public static void main(String[] args) {
+public class PropertyRentalApplication
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(PropertyRentalApplication.class, args);
     }
-
 }
